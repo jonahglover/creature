369f87776a90bf84eed6447e1bc2f7fdf89a933e
Jonah Glover <jonah@jonahglover.com>
2014-10-04 10:29:18 -0700

    added an abstract filter

---
+++ creature/lib/test.js
@@ -0,0 +1,15 @@
+AbstractFilter = require('./AbstractFilter.js');
+
+requestFilter = AbstractFilter;
+
+urls = ["google.com","facebook.com","yelp.com"];
+
+requestFilter.addRule("popular", function(url){
+	if(url == "google.com"){
+		return false;
+	}else{
+		return true;
+	}
+});
+
+requestFilter.filter(urls);
